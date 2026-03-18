0ffd913171cfaa4ebacb628d300588c12ae76ad6
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-18 17:05:34 +0100

    chore: Qt 6

--- infer_qwen2_5_vl_process.py
+++ infer_qwen2_5_vl_process.py
@@ -211,7 +211,7 @@ class InferQwen25VlFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Run vision-language model series based on Qwen2.5"
         # relative path -> as displayed in Ikomia Studio algorithm tree
         self.info.path = "Plugins/Python/VLM"
-        self.info.version = "1.1.1"
+        self.info.version = "1.2.0"
         self.info.icon_path = "images/icon.png"
         self.info.authors =     "Bai, Shuai and Chen, Keqin and Liu, Xuejing and Wang, Jialin and Ge, " \
                                 "Wenbin and Song, Sibo and Dang, Kai and Wang, Peng and Wang, Shijie and Tang, " \
@@ -225,7 +225,7 @@ class InferQwen25VlFactory(dataprocess.CTaskFactory):
         self.info.license = "Apache 2.0"
 
         # Ikomia API compatibility
-        self.info.min_ikomia_version = "0.13.0"
+        self.info.min_ikomia_version = "0.16.0"
 
         # Python compatibility
         self.info.min_python_version = "3.11.0"
